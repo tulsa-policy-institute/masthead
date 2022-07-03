263d9455bdff0411969756635cba6b7a559a0ef6
Matt Gardner <wilburnforce@gmail.com>
2022-07-03 14:49:01 -0500

    Update tostyles and lint issues

--- src/routes/Lecture.js
+++ src/routes/Lecture.js
@@ -1,4 +1,4 @@
-function Lecture() {
+function Lecture({ lecture }) {
   return <>
     Lecture
   </>;
--- src/routes/Search.js
+++ src/routes/Search.js
@@ -2,6 +2,10 @@ import React, { useState, useEffect } from 'react'
 import Select from 'react-select'
 import { useNavigate, Link } from 'react-router-dom';
 
+function randomize(list) {
+  return list.sort( () => Math.random() - 0.5)
+}
+
 function Search({ questions, lectures }) {
   const [selectedQuestion, setSelectedQuestion] = useState();
   const navigate = useNavigate();
@@ -24,15 +28,35 @@ function Search({ questions, lectures }) {
 
       navigate(`/lectures/${lecture.id}`);
     }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [filteredLectures]);
 
   return <>
-    <div className="">
+    <div className=''>
       <Select
+        styles={{
+          control: (provided) => ({
+            ...provided,
+            borderRadius: '9999px',
+            filter: 'drop-shadow(0 10px 8px rgb(0 0 0 / 0.04)) drop-shadow(0 4px 3px rgb(0 0 0 / 0.1))',
+            borderColor: '#fff',
+          }),
+        }}
+        placeholder=''
         options={questions.map(q => ({ value: q, label: q.Title }))}
         onChange={handleChange}
         isClearable={true}
       />
+      <div className='shadow-lg mt-4 rounded-2xl'>
+        <div className='border-b-gray-200 border-b'>
+          <h6 className='text-sm text-gray-400 m-1 p-3'>Suggested Searches</h6>
+        </div>
+        {randomize(questions).slice(0, 8).map((q) =>
+          <div key={q.id} onClick={() => handleChange({ value: q })} className='cursor-pointer hover:bg-slate-100 m-0 border-b-gray-200 border-b'>
+            <h3 className='text-lg m-1 p-3'>{q.Title}</h3>
+          </div>
+        )}
+      </div>
     </div>
     <div className="sm:grid sm:grid-cols-2 gap-8">
       {filteredLectures.map((lecture) =>
