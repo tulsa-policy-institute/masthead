c961219473d00fde9bfdcaf407f4cd2881382dbe
Matt Gardner <wilburnforce@gmail.com>
2022-08-31 11:52:03 -0700

    Update category tags

--- src/routes/Results.js
+++ src/routes/Results.js
@@ -98,7 +98,7 @@ const Results = ({ results, typedInput, cookies, isQuerying }) => {
       {tags.filter(t => Object.keys(CATEGORY_ICON_LOOKUP).includes(t)).map((c, i) => <div
         key={i}
         style={{ backgroundColor: selectedFilters.includes(c) ? TAG_COLOR_LOOKUP[c] : '' }}
-        className={`text-white select-none bg-gray-200/60 p-2 m-1 rounded-lg text-xs cursor-pointer`}
+        className={`text-white select-none bg-gray-200/20 p-2 m-1 rounded-lg text-xs cursor-pointer`}
         onClick={() => {
           if(selectedFilters.includes(c)) {
             gaFilteringTracker('unset', c);
