e34ebf5d576896012b9498247486f4ea58783346
Matt Gardner <wilburnforce@gmail.com>
2022-08-31 15:22:59 -0700

    Keep magnifying glass

--- src/routes/Search.js
+++ src/routes/Search.js
@@ -23,11 +23,10 @@ const TypeaheadSearch = withCookies(({ setTypedInput, className, typedInput = ''
     <label htmlFor="default-search" className="mb-2 text-sm font-medium text-gray-900 sr-only">Search</label>
     <div className="relative">
       <div className="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none z-10">
-        {children ? children :
-          <svg aria-hidden="true" className="w-6 h-6 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
-          </svg>
-        }
+        <svg aria-hidden="true" className="m-1 w-6 h-6 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
+        </svg>
+        {children}
       </div>
       <div
         className='bg-transparent tpi-blur'
@@ -43,7 +42,7 @@ const TypeaheadSearch = withCookies(({ setTypedInput, className, typedInput = ''
           onChange={(e) => setTypedInput(e.target.value)}
           type="text"
           id="default-search"
-          className="p-4 pl-10 w-full text-sm bg-transparent rounded-3xl border-0 text-white focus:outline-none"
+          className="p-4 pl-[4.5rem] w-full text-sm bg-transparent rounded-3xl border-0 text-white focus:outline-none"
           onKeyDown={(e) => {
             if (e.key === 'Enter') {
               doSearch();
